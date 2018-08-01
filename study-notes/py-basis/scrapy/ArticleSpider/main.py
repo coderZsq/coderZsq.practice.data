9183218d68d65cfb2b98ee759292f8e6a32c5d5b
coderZsq <a13701777868@yahoo.com>
2018-08-01 14:09:28 +0800

    add

---
+++ study-notes/py-basis/scrapy/ArticleSpider/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'castiel'
+
+from scrapy.cmdline import execute
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(['scrapy', 'crawl', 'jobbole'])
