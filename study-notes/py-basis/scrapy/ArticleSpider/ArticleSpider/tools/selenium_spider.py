d4b89aae0a0f45ead21802d7195f38c2007110fd
coderZsq <a13701777868@yahoo.com>
2018-08-07 10:29:12 +0800

    add

--- study-notes/py-basis/scrapy/ArticleSpider/ArticleSpider/tools/selenium_spider.py
+++ study-notes/py-basis/scrapy/ArticleSpider/ArticleSpider/tools/selenium_spider.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from selenium import webdriver
 from scrapy import Selector
 
-browser = webdriver.Firefox(
-    executable_path='/Users/zhushuangquan/Native Drive/GitHub/coderZsq.practice.data/study-notes/py-basis/scrapy/ArticleSpider/ArticleSpider/tools/geckodriver')
-browser.get('https://taobao.com')
-print (browser.page_source)
+import time
+from selenium import webdriver
 
-t_selector = Selector(text=browser.page_source)
-t_selector.css('.tm-promo-price .tm-price::text').extract()
+# Optional argument, if not specified will search path.
+driver = webdriver.Chrome(
+    '/Users/zhushuangquan/Native Drive/GitHub/coderZsq.practice.data/study-notes/py-basis/scrapy/ArticleSpider/ArticleSpider/tools/chromedriver')
+driver.get('http://www.google.com/xhtml')
+time.sleep(5)  # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5)  # Let the user actually see something!
+driver.quit()
 
-browser.get('https://www.zhihu.com/#signin')
-browser.find_element_by_css_selector('.view-signin input[name="account"]').send_keys('18782902568')
-browser.find_element_by_css_selector('.view-signin input[name="password"]').send_keys('admin123')
-browser.find_element_by_css_selector('.view-signin button.sign-button').click()
+# t_selector = Selector(text=browser.page_source)
+# t_selector.css('.tm-promo-price .tm-price::text').extract()
 
-browser.quit()
+# browser.get('https://www.zhihu.com/#signin')
+# import time
+# time.sleep(10)
+# browser.find_element_by_css_selector('.view-signin input[name="account"]').send_keys('18782902568')
+# browser.find_element_by_css_selector('.view-signin input[name="password"]').send_keys('admin123')
+# browser.find_element_by_css_selector('.view-signin button.sign-button').click()
+
