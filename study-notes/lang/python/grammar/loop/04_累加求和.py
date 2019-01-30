61d6d73885a9e118ba64c93224e0221a0972d1e8
coderZsq <a13701777868@yahoo.com>
2019-01-30 17:46:36 +0800

    add

---
+++ study-notes/lang/python/grammar/loop/04_累加求和.py
@@ -0,0 +1,10 @@
+result = 0
+
+i = 0
+
+while i <= 100:
+    print(i)
+    result += i
+    i += 1
+
+print("0~100之间的数字求和结果 = %d" % result)
---
+++ study-notes/lang/python/grammar/loop/10_嵌套打印小星星.py
@@ -0,0 +1,11 @@
+row = 1
+
+while row <= 5:
+    col = 1
+    while col <= row:
+        # print("%d" % col)
+        print("*", end="")
+        col += 1
+    # print("第%d行" % row)
+    print("")
+    row += 1
