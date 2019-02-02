d455078fd84b31eed2f4b6834ca24cbb7d46ea2c
coderZsq <a13701777868@yahoo.com>
2019-02-02 10:32:39 +0800

    add

--- study-notes/lang/python/grammar/14_game/plane_main.py
+++ study-notes/lang/python/grammar/14_game/plane_main.py
@@ -4,6 +4,7 @@ from plane_sprites import *
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
 FRAME_PER_SEC = 60
 CREATE_ENEMY_EVENT = pygame.USEREVENT
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
 
 
 class PlaneGame(object):
@@ -13,12 +14,15 @@ class PlaneGame(object):
         self.clock = pygame.time.Clock()
         self.__create_sprites()
         pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
+        pygame.time.set_timer(HERO_FIRE_EVENT, 500)
 
     def __create_sprites(self):
         bg1 = Background()
         bg2 = Background(True)
         self.back_group = pygame.sprite.Group(bg1, bg2)
         self.enemy_group = pygame.sprite.Group()
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
 
     def start_game(self):
         print("游戏开始")
@@ -37,15 +41,34 @@ class PlaneGame(object):
                 print("敌机出场...")
                 enemy = Enemy()
                 self.enemy_group.add(enemy)
+            elif event.type == HERO_FIRE_EVENT:
+                self.hero.fire()
+            # elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            #     print("向右移动...")
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_RIGHT]:
+            self.hero.speed = 10
+        elif keys_pressed[pygame.K_LEFT]:
+            self.hero.speed = -10
+        else:
+            self.hero.speed = 0
 
     def __check_collide(self):
-        pass
+        pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group, True, True)
+        enemies = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
+        if len(enemies) > 0:
+            self.hero.kill()
+            PlaneGame.__game_over()
 
     def __update_sprites(self):
         self.back_group.update()
         self.back_group.draw(self.screen)
         self.enemy_group.update()
         self.enemy_group.draw(self.screen)
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
 
     @staticmethod
     def __game_over():
--- study-notes/lang/python/grammar/14_game/plane_sprites.py
+++ study-notes/lang/python/grammar/14_game/plane_sprites.py
@@ -32,7 +32,7 @@ class Background(GameSprite):
 class Enemy(GameSprite):
     def __init__(self):
         super().__init__("./images/enemy1.png")
-        self.speed = random.randint(1, 3)
+        self.speed = random.randint(3, 5)
         self.rect.bottom = 0
         max_x = SCREEN_RECT.width - self.rect.width
         self.rect.x = random.randint(0, max_x)
@@ -45,3 +45,39 @@ class Enemy(GameSprite):
 
     def __del__(self):
         print("敌机挂了 %s" % self.rect)
+
+
+class Hero(GameSprite):
+    def __init__(self):
+        super().__init__("./images/me1.png", 0)
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom - 120
+        self.bullets = pygame.sprite.Group()
+
+    def update(self, *args):
+        self.rect.x += self.speed
+        if self.rect.x < 0:
+            self.rect.x = 0
+        elif self.rect.right > SCREEN_RECT.right:
+            self.rect.right = SCREEN_RECT.right
+
+    def fire(self):
+        print("发射子弹...")
+        for i in (0, 1, 2):
+            bullet = Bullet()
+            bullet.rect.bottom = self.rect.y - i * 20
+            bullet.rect.centerx = self.rect.centerx
+            self.bullets.add(bullet)
+
+
+class Bullet(GameSprite):
+    def __init__(self):
+        super().__init__("./images/bullet1.png", -8.5)
+
+    def update(self, *args):
+        super().update()
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def __del__(self):
+        print("子弹被销毁...")
