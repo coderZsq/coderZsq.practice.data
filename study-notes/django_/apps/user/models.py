e511e66886a7713117814633fc6da51088f6c7bd
coderZsq <a13701777868@yahoo.com>
2019-04-10 14:13:52 +0800

    add

--- study-notes/django_/apps/user/models.py
+++ study-notes/django_/apps/user/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from db.base_model import BaseModel
+# Create your models here.
 
 
 class User(AbstractUser, BaseModel):
+    '''用户模型类'''
+
     class Meta:
         db_table = 'df_user'
         verbose_name = '用户'
@@ -11,6 +14,7 @@ class User(AbstractUser, BaseModel):
 
 
 class Address(BaseModel):
+    '''地址模型类'''
     user = models.ForeignKey('User', verbose_name='所属账户')
     receiver = models.CharField(max_length=20, verbose_name='收件人')
     addr = models.CharField(max_length=256, verbose_name='收件地址')
--- study-notes/django_/django_/urls.py
+++ study-notes/django_/django_/urls.py
@@ -1,27 +1,15 @@
-"""django_ URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import include, path
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tinymce/', include('tinymce.urls')),
-    path('user/', include('user.urls', namespace='user')),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('order/', include('order.urls', namespace='order')),
-    path('goods/', include('goods.urls', namespace='goods')),
+    # Examples:
+    # url(r'^$', 'django_.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^tinymce/', include('tinymce.urls')), # 富文本编辑器
+    url(r'^user/', include('user.urls', namespace='user')), # 用户模块
+    url(r'^cart/', include('cart.urls', namespace='cart')), # 购物车模块
+    url(r'^order/', include('order.urls', namespace='order')), # 订单模块
+    url(r'^', include('goods.urls', namespace='goods')), # 商品模块
 ]
