a0e7f0c7b5893d4dc20ca5113b581df71008f823
coderZsq <a13701777868@yahoo.com>
2019-04-15 11:55:14 +0800

    add

--- study-notes/django_/apps/goods/urls.py
+++ study-notes/django_/apps/goods/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from apps.goods.views import IndexView
+from apps.goods.views import IndexView, DetailView
 
 urlpatterns = [
-    url(r'^index$', IndexView.as_view(), name='index')
+    url(r'^index$', IndexView.as_view(), name='index'),
+    url(r'^goods/(?P<goods_id>\d+)$', DetailView.as_view(), name='detail')
 ]
--- study-notes/django_/apps/goods/views.py
+++ study-notes/django_/apps/goods/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from django.views.generic import View
 from django.core.cache import cache
-from apps.goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+from apps.goods.models import GoodsType, GoodsSKU, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 from django_redis import get_redis_connection
+from apps.order.models import OrderGoods
 # Create your views here.
 
 
@@ -33,3 +35,33 @@ class IndexView(View):
             cart_count = conn.hlen(cart_key)
         context.update(cart_count=cart_count)
         return render(request, 'index.html', context)
+
+
+class DetailView(View):
+    @staticmethod
+    def get(request, goods_id):
+        try:
+            sku = GoodsSKU.objects.get(id=goods_id)
+        except GoodsSKU.DoesNotExist:
+            return redirect(reverse('goods:index'))
+        types = GoodsType.objects.all()
+        sku_orders = OrderGoods.objects.filter(sku=sku).exclude(comment='')
+        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')[:2]
+        same_spu_skus = GoodsSKU.objects.filter(goods=sku.goods).exclude(id=goods_id)
+        user = request.user
+        cart_count = 0
+        if user.is_authenticated():
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d' % user.id
+            cart_count = conn.hlen(cart_key)
+            conn = get_redis_connection('default')
+            history_key = 'history_%d' % user.id
+            conn.lrem(history_key, 0, goods_id)
+            conn.lpush(history_key, goods_id)
+            conn.ltrim(history_key, 0, 4)
+        context = {'sku': sku, 'types': types,
+                   'sku_orders': sku_orders,
+                   'new_skus': new_skus,
+                   'same_spu_skus': same_spu_skus,
+                   'cart_count': cart_count}
+        return render(request, 'detail.html', context)
