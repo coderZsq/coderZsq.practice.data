87b4128d31a415419cd07eac2266021d1813d0d7
coderZsq <a13701777868@yahoo.com>
2019-04-16 17:21:53 +0800

    add

--- study-notes/django_/apps/order/urls.py
+++ study-notes/django_/apps/order/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
-from apps.order.views import OrderPlaceView, OrderCommitView
+from apps.order.views import OrderPlaceView, OrderCommitView, OrderPayView, CheckPayView, CommentView
 
 urlpatterns = [
     url(r'^place$', OrderPlaceView.as_view(), name='place'),
     url(r'^commit$', OrderCommitView.as_view(), name='commit'),
+    url(r'^pay$', OrderPayView.as_view(), name='pay'),
+    url(r'^check$', CheckPayView.as_view(), name='check'),
+    url(r'^comment/(?P<order_id>.+)$', CommentView.as_view(), name='comment'),
 ]
