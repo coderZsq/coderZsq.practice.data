92693157233aac80bdbd21af727aafe22d0542ef
coderZsq <a13701777868@yahoo.com>
2019-05-24 19:41:23 +0800

    add

--- study-notes/python/21_django/apps/cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from apps.cart.views import CartAddView, CartInfoView, CartUpdateView, CartDeleteView
-
-urlpatterns = [
-    url(r'^add$', CartAddView.as_view(), name='add'),
-    url(r'^$', CartInfoView.as_view(), name='show'),
-    url(r'^update$', CartUpdateView.as_view(), name='update'),
-    url(r'^delete$', CartDeleteView.as_view(), name='delete'),
-]
--- study-notes/python/22_flask/flask_/api_1_0/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding:utf-8
-
-from flask import Blueprint
-
-api = Blueprint("api_1_0", __name__)
-
-from . import index, verify_code
